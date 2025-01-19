eeaf0c6838692c49a6e23ac56cd82b1cf4e39225
AndreasDelabie <andreas.delabie@gmail.com>
2025-01-19 16:43:24 +0100

    Added keyboard listener in other thread

--- src/screen_reeder/main.py
+++ src/screen_reeder/main.py
@@ -1,14 +1,24 @@
-import os, tkinter
+import os, tkinter, keyboard, threading
 from gtts import gTTS
 from playsound import playsound
 
 
+def clear_console():
+    osname = os.name
+    if osname == 'nt':
+        _ = os.system('cls') # For Windows
+    else:
+        _ = os.system('clear') # For macOS & Linux
+
+
 def __init__():
+    clear_console()
     print('-------------\nScreen-Reeder\n-------------')
 
     global language, tempfile
     language = input('Input language (ex. nl, en, fr): ')
-    print(f"Talking in '{language}' now...")
+    print(f"Reading in '{language}' now...")
+    print('Press CTRL+ALT+L to change language')
 
     userpath = os.path.expanduser('~')
     temppath = os.path.join(userpath, '.screen-reeder\\temp')
@@ -35,6 +45,15 @@ def remove_old_file():
         pass
 
 
+def get_keypress():
+    while True:
+        if keyboard.is_pressed('ctrl+alt+l'):
+            __init__()
+        else:
+            continue
+thread_get_keypress = threading.Thread(target=get_keypress, name='Get keypresses')
+
+
 def play_selected_text():
     text_previous = None
 
@@ -49,6 +68,8 @@ def play_selected_text():
             playsound(tempfile)
             text_previous = text_selected
 
+
 def main():
     __init__()
+    thread_get_keypress.start()
     play_selected_text()
